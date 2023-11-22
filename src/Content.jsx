b5204ac68fe1655b3d0295e982a8290925e169eb
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-21 16:54:28 -0800

    added Modal.css and Modal.jsx, added handleShow and handleClose functions into Content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,23 @@
-export function Content() {
-  return (
-    
+import { Modal } from "./Modal";
 
+export function Content() {
+  const [isJobsShowVisible, setIsJobsShowVisible] = useState(false);
+  const [currentJob, setCurrentJob] = useState({});
+
+  const handleShowJob = (job) => {
+    console.log("handleShowJob", job);
+    setIsJobsShowVisible(true);
+    setCurrentJob(Job);
+
+    const handleClose = () => {
+      console.log("handleClose");
+      setIsJobsShowVisible(false);
+    };
+  };
+
+  return (
+    <Modal show={true}>
+      <h1>Test</h1>
+    </Modal>
   );
 }
