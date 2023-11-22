deb46d56c8c10aac63a7cb5e11eb941d881fb3b9
Levi Powis <lpowis97@gmail.com>
2023-11-21 20:24:22 -0500

    adds JobsIndex.jsx and handlerIndexJobs to Content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,23 @@
-export function Content() {
-  return (
-    
+import { JobsIndex } from "./JobsIndex";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
+export function Content() {
+  const [jobs, setJobs] = useState([]);
+
+  const handleIndexJobs = () => {
+    console.log("handleIndexJobs");
+    axios.get("http://localhost:3000/jobs.json").then((response) => {
+      console.log(response.data);
+      setJobs(response.data);
+    });
+  };
+
+  useEffect(handleIndexJobs, []);
+
+  return (
+    <div>
+      <JobsIndex jobs={jobs} />
+    </div>
   );
 }
