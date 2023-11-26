e5527d6b00e94b5c35b92ca3ecfb7dffb549db11
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-26 11:50:53 -0800

    fixed jobs update

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,56 @@
 export function Header() {
   return (
-    <header>
-      <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
-      </nav>
-    </header>
+    // <header>
+    //   <nav>
+    //     <a href="#">Home</a> | <a href="#">Link</a>
+    //   </nav>
+    // </header>
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/jobs">
+                All Jobs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/jobs/new">
+                New Jobs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/signup">
+                Sign Up
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/login">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
